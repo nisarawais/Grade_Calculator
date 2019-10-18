97f6a0a5a1891cc9cc05f5455fb5fb07185a7f3d
Awais <nisarawais246@gmail.com>
2019-10-17 21:10:51 -0400

    final code!!!!! (:

--- src/GradeCenter.java
+++ src/GradeCenter.java
@@ -11,13 +11,13 @@ public class GradeCenter
         int tracker = 1; // declaring the value for tracker. it helps to identify which number is at
         int numberint = 1; // this will be the number that display on the screen for the course
         String number = "";
-        int grade1 = 0;
-        int grade2 = 0;
-        int grade3= 0;
-        int grade4= 0;
-        int grade5 = 0;
-        int grade6= 0;
-        int temporarygrade; // temporarily stores the value for grade
+        double grade1 = 0;
+        double grade2 = 0;
+        double grade3= 0;
+        double grade4= 0;
+        double grade5 = 0;
+        double grade6= 0;
+        double temporarygrade; // temporarily stores the value for grade
 
 
         // activates the scanner for the user to input their response
@@ -39,11 +39,7 @@ public class GradeCenter
                 temporarygrade = keyboard.nextInt();
 
 
-                while (temporarygrade < 0 && temporarygrade > 100) {
-                    System.out.printf("The grade must be in the range of 0-100 %n");
-                    System.out.printf("Please enter your grade for %d%s course:  ", numberint, number);
-                    temporarygrade = keyboard.nextInt();
-                }
+                checkerPercentage(temporarygrade,numberint,number);
 
                 grade1 = temporarygrade;
 
@@ -58,11 +54,7 @@ public class GradeCenter
                 temporarygrade = keyboard.nextInt();
 
 
-                while (temporarygrade < 0 && temporarygrade > 100) {
-                    System.out.printf("The grade must be in the range of 0-100 %n");
-                    System.out.printf("Please enter your grade for %d%s course:  ", numberint, number);
-                    temporarygrade = keyboard.nextInt();
-                }
+                checkerPercentage(temporarygrade,numberint,number);
 
                 grade2 = temporarygrade;
             }
@@ -75,13 +67,7 @@ public class GradeCenter
                 System.out.printf("Please enter your grade for %d%s course:  ", numberint, number);
                 temporarygrade = keyboard.nextInt();
 
-
-                while (temporarygrade < 0 && temporarygrade > 100) {
-                    System.out.printf("The grade must be in the range of 0-100 %n");
-                    System.out.printf("Please enter your grade for %d%s course:  ", numberint, number);
-                    temporarygrade = keyboard.nextInt();
-                }
-
+                checkerPercentage(temporarygrade,numberint,number);
                 grade3 = temporarygrade;
             }
 
@@ -94,11 +80,8 @@ public class GradeCenter
                 temporarygrade = keyboard.nextInt();
 
 
-                while (temporarygrade < 0 && temporarygrade > 100) {
-                    System.out.printf("The grade must be in the range of 0-100 %n");
-                    System.out.printf("Please enter your grade for %d%s course:  ", numberint, number);
-                    temporarygrade = keyboard.nextInt();
-                }
+                checkerPercentage(temporarygrade,numberint,number);
+
                 grade4 = temporarygrade;
             }
 
@@ -110,12 +93,7 @@ public class GradeCenter
                 System.out.printf("Please enter your grade for %d%s course:  ", numberint, number);
                 temporarygrade = keyboard.nextInt();
 
-
-                while (temporarygrade < 0 && temporarygrade > 100) {
-                    System.out.printf("The grade must be in the range of 0-100 %n");
-                    System.out.printf("Please enter your grade for %d%s course:  ", numberint, number);
-                    temporarygrade = keyboard.nextInt();
-                }
+                checkerPercentage(temporarygrade,numberint,number);
 
                 grade5 = temporarygrade;
             }
@@ -128,12 +106,8 @@ public class GradeCenter
                 System.out.printf("Please enter your grade for %d%s course:  ", numberint, number);
                 temporarygrade = keyboard.nextInt();
 
+                checkerPercentage(temporarygrade,numberint,number);
 
-                while (temporarygrade < 0 && temporarygrade > 100) {
-                    System.out.printf("The grade must be in the range of 0-100 %n");
-                    System.out.printf("Please enter your grade for %d%s course:  ", numberint, number);
-                    temporarygrade = keyboard.nextInt();
-                }
 
                 grade6 = temporarygrade;
             }
@@ -148,20 +122,19 @@ public class GradeCenter
         // displaying the grade for each courses and averaging the all courses
 
 
-        System.out.printf("The mark for the %d%s course is %d% which is %s%n",numberint,number,grade1,convertingtoGrade(grade1));
-        System.out.printf("The mark for the %d%s course is %d% which is %s%n",numberint,number,grade2,convertingtoGrade(grade2));
-        System.out.printf("The mark for the %d%s course is %d% which is %s%n",numberint,number,grade3,convertingtoGrade(grade3));
-        System.out.printf("The mark for the %d%s course is %d% which is %s%n",numberint,number,grade4,convertingtoGrade(grade4));
-        System.out.printf("The mark for the %d%s course is %d% which is %s%n",numberint,number,grade5,convertingtoGrade(grade5));
-        System.out.printf("The mark for the %d%s course is %d% which is %s%n",numberint,number,grade6,convertingtoGrade(grade6));
+        System.out.printf("The mark for the %d%s course is %.1f%% which is %s%n",numberint,number,grade1,convertingtoGrade(grade1));
+        System.out.printf("The mark for the %d%s course is %.1f%% which is %s%n",numberint,number,grade2,convertingtoGrade(grade2));
+        System.out.printf("The mark for the %d%s course is %.1f%% which is %s%n",numberint,number,grade3,convertingtoGrade(grade3));
+        System.out.printf("The mark for the %d%s course is %.1f%% which is %s%n",numberint,number,grade4,convertingtoGrade(grade4));
+        System.out.printf("The mark for the %d%s course is %.1f%% which is %s%n",numberint,number,grade5,convertingtoGrade(grade5));
+        System.out.printf("The mark for the %d%s course is %.1f%% which is %s%n",numberint,number,grade6,convertingtoGrade(grade6));
 
-        System.out.printf("The average mark is %d% which is %s",averageGrade(grade1,grade2,grade3,grade4,grade5,grade6),convertingtoGrade((int) averageGrade(grade1,grade2,grade3,grade4,grade5,grade6)));
-
+        System.out.printf("The average mark is %.1f%% which is %s",averageGrade(grade1,grade2,grade3,grade4,grade5,grade6),convertingtoGrade(averageGrade(grade1,grade2,grade3,grade4,grade5,grade6)));
 
     }
 
     // matching the percentage to letter grade
-    public static String convertingtoGrade (int grade)
+    public static String convertingtoGrade (double grade)
     {
         if (grade <= 49)
         {
@@ -210,9 +183,24 @@ public class GradeCenter
         }
     }
 
-    public static double averageGrade(int grade1, int grade2, int grade3, int grade4,int grade5, int grade6)
+    public static double averageGrade(double grade1, double grade2, double grade3, double grade4, double grade5, double grade6)
     {
-        return (double) (grade1+grade2+grade3+grade4+grade5+grade6)/6;
+        return (grade1+grade2+grade3+grade4+grade5+grade6)/6;
+    }
+
+    public static double checkerPercentage(double grade, int numberint,String number)
+    {
+        // activates the scanner for the user to input their response
+        Scanner keyboard = new Scanner(System.in);
+
+        while (grade < 0 || grade > 100) {
+            System.out.printf("The grade must be in the range of 0-100 %n");
+            System.out.printf("Please enter your grade for %d%s course:  ", numberint, number);
+            grade = keyboard.nextDouble();
+        }
+
+        return grade;
+
     }
 
 }
